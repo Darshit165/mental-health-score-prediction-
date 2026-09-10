a5a9a2092bbe11b91a3fff34ac968ac302b1eba9
Darshit165 <darshitp2211@gmail.com>
2026-09-10 12:22:33 +0530

    load data

---
+++ mentalhealth.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df= pd.read_csv('D:\project\mental health score prediction\Student Social Media And Mental Health Impact.csv')
+print(df.head())
